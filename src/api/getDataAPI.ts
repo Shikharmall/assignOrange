559da581bafaca6e365673eb89a7244c703fa1e7
Shikhar Mall <shmall.21.2020@gmail.com>
2025-07-30 21:28:58 +0530

    new commit

--- src/api/getDataAPI.ts
+++ src/api/getDataAPI.ts
@@ -5,6 +5,8 @@ import { API_URL_BASE } from "../utils/apiURL";
 
 export const getDataAPI = async (page: number, limit: number) => {
 
+  console.log(API_URL_BASE);
+
   try {
     let result = await axios(
       `${API_URL_BASE}/artworks?page=${page}&limit=${limit}`,
